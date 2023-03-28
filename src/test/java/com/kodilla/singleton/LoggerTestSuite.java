8413613deb9251ee57463daf19ab999c2e0c3ee9
Przemyslaw Pietrasz <przemek681@gmail.com>
2023-03-29 00:23:50 +0200

    11.2

--- src/test/java/com/kodilla/singleton/LoggerTestSuite.java
+++ src/test/java/com/kodilla/singleton/LoggerTestSuite.java
@@ -1,2 +1,23 @@
-package com.kodilla.singleton;public class LoggerTestSuite {
+package com.kodilla.singleton;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LoggerTestSuite {
+
+
+    @Test
+    public void getLastLogTest(){
+        //Given
+        String log = "1st log";
+        String log2 = "2nd log";
+        //When
+        Logger.getInstance().log(log);
+        Logger.getInstance().log(log2);
+        //Then
+        Assertions.assertEquals(log2, Logger.getInstance().getLastLog());
+    }
 }
+
+
+
